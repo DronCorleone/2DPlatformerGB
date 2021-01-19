f95f643233b9b7d0b831f86bdeb8623afdd1090c
Dron Corleone <kefirchik13@gmail.com>
2021-01-19 16:07:15 +0300

    Player move controller base

---
+++ Assets/Scripts/Controller/PlayerMoveController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveController
+{
+    private CharacterView _characterView;
+    private SpriteAnimator _spriteAnimator;
+    private float xAxisInput = 0;
+    private bool doJump = false;
+
+    private string _verticalAxis = "Vertical";
+    private string _horizontalAxis = "Horizontal";
+
+    public PlayerMoveController (CharacterView characterView, SpriteAnimator spriteAnimator)
+    {
+        _characterView = characterView;
+        _spriteAnimator = spriteAnimator;
+    }
+
+    public void Update()
+    {
+        doJump = Input.GetAxis(_verticalAxis) > 0;
+        xAxisInput = Input.GetAxis(_horizontalAxis);
+    }
+}
