366422b302d5fc9076acfd8ac183c45066b1342f
DronCorleone <48313301+DronCorleone@users.noreply.github.com>
2021-02-02 20:49:58 +0300

    Adding enemies sprites + make spike sphere with joint

---
+++ Assets/Scripts/Triggers/DeathTrigger.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
+public class DeathTrigger : MonoBehaviour
+{
+    private Main _main;
+
+    private void Start()
+    {
+        _main = FindObjectOfType<Main>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<CharacterView>())
+        {
+            _main.EndGame(false);
+        }
+    }
+}
